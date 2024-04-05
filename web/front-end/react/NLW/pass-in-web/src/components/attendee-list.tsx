b6425d9f756473abe16b93413e65fe3c61ee3d59
Gleybson Farias <gleybson.farias@gmail.com>
2024-04-05 16:48:22 -0300

    feat: custom data table

--- web/front-end/react/NLW/pass-in-web/src/components/attendee-list.tsx
+++ web/front-end/react/NLW/pass-in-web/src/components/attendee-list.tsx
@@ -5,9 +5,9 @@ export function AttendeeList() {
         <div className='flex flex-col gap-4'>
             <div className="flex gap-3 items-center">            
                 <h1 className="text-2xl font-bold">Participantes</h1>
-                <div className="px-3 w-72 py-1.5 border border-white/10 bg-transparent rounded-lg text-sm flex items-center gap-3">
+                <div className="px-3 w-72 py-1.5 border border-white/10 bg-transparent rounded-lg flex items-center gap-3">
                     <Search className="size-4 text-emerald-300"/>
-                    <input className="bg-transparent flex-1 outline-none" placeholder="Buscar Participantes..."/>
+                    <input className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar Participantes..."/>
                     </div>            
             </div>
 
@@ -16,7 +16,7 @@ export function AttendeeList() {
                     <thead>
                         <tr className='border-b border-white/10'>
                             <th style={{ width: 48 }} className='py-3 px-4 text-sm font-semibold text-left'>
-                                <input type='checkbox'/>
+                                <input type='checkbox' className='size-4 bg-black/20 rounded  border border-white/10 '  />
                             </th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>
                                 Código
@@ -38,9 +38,9 @@ export function AttendeeList() {
                         <tbody>
                             { Array.from ({ length: 10}).map( (_, index)=> {
                                 return (
-                                    <tr key={index} className='border-b border-white/10'>
+                                    <tr key={index} className='border-b border-white/10 hover:bg-white/5'>
                                         <td className='py-3 px-4 text-sm text-zinc-300'>
-                                            <input type='checkbox'/>
+                                            <input type='checkbox' className='size-4 bg-black/20 rounded  border border-white/10'  />
                                         </td>
                                         <td className='py-3 px-4 text-sm text-zinc-300'>
                                             123213
