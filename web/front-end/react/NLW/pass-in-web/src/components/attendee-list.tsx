98c399a0fd82f44b567a0df2ffb8bbad3fcfca71
Gleybson Farias <gleybson.farias@gmail.com>
2024-04-05 16:12:08 -0300

    feat: add new custom to buttons

--- web/front-end/react/NLW/pass-in-web/src/components/attendee-list.tsx
+++ web/front-end/react/NLW/pass-in-web/src/components/attendee-list.tsx
@@ -1,4 +1,4 @@
-import { Search} from 'lucide-react'
+import { ChevronLeft, ChevronRight, ChevronsLeft, ChevronsRight, MoreHorizontal, Search} from 'lucide-react'
 
 export function AttendeeList() {
     return (
@@ -15,7 +15,7 @@ export function AttendeeList() {
                 <table className='w-full border border-white/10 rounded-lg'>
                     <thead>
                         <tr className='border-b border-white/10'>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'>
+                            <th style={{ width: 48 }} className='py-3 px-4 text-sm font-semibold text-left'>
                                 <input type='checkbox'/>
                             </th>
                             <th className='py-3 px-4 text-sm font-semibold text-left'>
@@ -30,15 +30,15 @@ export function AttendeeList() {
                             <th className='py-3 px-4 text-sm font-semibold text-left'>
                                 Data do check-in
                             </th>
-                            <th className='py-3 px-4 text-sm font-semibold text-left'>
+                            <th style={{ width: 64 }} className='py-3 px-4 text-sm font-semibold text-left'>
                                 botão
                             </th>
                         </tr>
                         </thead>
                         <tbody>
-                            { Array.from ({ length: 10}).map( ()=> {
+                            { Array.from ({ length: 10}).map( (_, index)=> {
                                 return (
-                                    <tr className='border-b border-white/10'>
+                                    <tr key={index} className='border-b border-white/10'>
                                         <td className='py-3 px-4 text-sm text-zinc-300'>
                                             <input type='checkbox'/>
                                         </td>
@@ -53,7 +53,11 @@ export function AttendeeList() {
                                         </td>
                                         <td className='py-3 px-4 text-sm text-zinc-300'>7 dias atras</td>
                                         <td className='py-3 px-4 text-sm text-zinc-300'>3 dias atras</td>
-                                        <td className='py-3 px-4 text-sm text-zinc-300'>Botão</td>
+                                        <td className='py-3 px-4 text-sm text-zinc-300'>
+                                            <button className='bg-black/20 border border-white/10 rounded-md p-1.5'>
+                                                <MoreHorizontal className='size-4'/>
+                                            </button>
+                                        </td>
                                     </tr>
                                 )
                             })}                            
@@ -61,7 +65,25 @@ export function AttendeeList() {
                         <tfoot>
                             <tr>
                                 <td className='py-3 px-4 text-sm text-zinc-300' colSpan={3}>Mostrando 10 de 228 itens</td>
-                                <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>Página 1 de 23</td>
+                                <td className='py-3 px-4 text-sm text-zinc-300 text-right' colSpan={3}>
+                                    <div className='inline-flex items-center gap-8'>
+                                    <span>Página 1 de 23</span>
+                                        <div className='flex gap-1.5'>
+                                            <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                                <ChevronsLeft className='size-4'/>
+                                            </button>
+                                            <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                                <ChevronLeft className='size-4'/>
+                                            </button>
+                                            <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                                <ChevronRight className='size-4'/>
+                                            </button>
+                                            <button className='bg-white/10 border border-white/10 rounded-md p-1.5'>
+                                                <ChevronsRight className='size-4'/>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </td>
                             </tr>
                         </tfoot>
                     
