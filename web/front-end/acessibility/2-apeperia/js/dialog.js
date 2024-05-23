eb0b19d414afb49fcbc8d42d8a232343300b5a4c
Gleybson Farias <gleybson.farias@gmail.com>
2024-05-22 21:21:05 -0300

    Merge branch 'main' of ithub.com:gleybsonf/my-studies

--- web/front-end/acessibility/2-apeperia/js/dialog.js
+++ web/front-end/acessibility/2-apeperia/js/dialog.js
@@ -1,18 +1,26 @@
-// Variáveis
+// Variáveis,
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog')
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
+btnAbreDialog.style.display = 'block';
+
+
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.emailNewsletter').focus();
+  conteudoForaDialog.inert = true
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');  
+  conteudoForaDialog.inert = false
+  btnAbreDialog.focus();   
 }
 
 // Listeners
